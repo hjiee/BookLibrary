9f3c7eba973a66b29c77ea1567170d9bc9ecda88
hyojin <jin33032@gmail.com>
2019-12-01 11:21:27 +0900

    ADD [ Setting 뷰 구현 - 앱 설정 정보 및 관리 정보 기능 추가 ]

--- app/src/main/java/com/hyden/booklibrary/view/setting/SettingFragment.kt
+++ app/src/main/java/com/hyden/booklibrary/view/setting/SettingFragment.kt
@@ -1,18 +1,69 @@
 package com.hyden.booklibrary.view.setting
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.hyden.base.BaseFragment
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.hyden.booklibrary.BuildConfig
 import com.hyden.booklibrary.R
-import com.hyden.booklibrary.databinding.FragmentHomeBinding
-import com.hyden.booklibrary.databinding.FragmentSettingBinding
 
-class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
+class SettingFragment : PreferenceFragmentCompat() {
 
-   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
         return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.setting, rootKey)
+        setSendToMail()
+        setSendToEvaluation()
+        setSendToDonate()
+        preferenceManager.context.setTheme(R.style.PreferenceTheme)
+    }
+
+    private fun setSendToMail() {
+        findPreference<Preference>(getString(R.string.setting_key_question))?.apply {
+            setOnPreferenceClickListener {
+                Intent(Intent.ACTION_SEND).apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_EMAIL, resources.getStringArray(R.array.email))
+                    putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+                    putExtra(
+                        Intent.EXTRA_TEXT,
+                        "모델명 : ${Build.MODEL}\n" +
+                                "OS버전 : ${Build.VERSION.RELEASE}\n" +
+                                "앱버전 : ${BuildConfig.VERSION_NAME}\n " +
+                                "-----------------------------------------\n\n"
+                    )
+                    startActivity(this)
+                }
+                true
+            }
+        }
+    }
+
+    private fun setSendToEvaluation() {
+        findPreference<Preference>(getString(R.string.setting_key_version))?.apply {
+            summary = "앱 버전 : ${BuildConfig.VERSION_NAME}"
+            this.context.setTheme(R.style.PreferenceTheme)
+        }
+    }
+
+    private fun setSendToDonate() {
+        findPreference<Preference>(getString(R.string.setting_key_donate))?.apply {
+            setOnPreferenceClickListener {
+                true
+            }
+        }
     }
 
     companion object {
