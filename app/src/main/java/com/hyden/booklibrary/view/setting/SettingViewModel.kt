f03f2adb707acb32469903024a09b1ae2ac9687b
hyojin <jin33032@gmail.com>
2020-01-10 20:36:56 +0900

    UPDATE [ 구글 로그인, 로그아웃 기능 추가 ]

--- app/src/main/java/com/hyden/booklibrary/view/setting/SettingViewModel.kt
+++ app/src/main/java/com/hyden/booklibrary/view/setting/SettingViewModel.kt
@@ -1,7 +1,13 @@
 package com.hyden.booklibrary.view.setting
 
 import com.hyden.base.BaseViewModel
+import com.hyden.booklibrary.data.repository.FirebaseRepository
+import com.hyden.booklibrary.data.repository.source.FirebaseDataSource
 
-class SettingViewModel : BaseViewModel() {
+class SettingViewModel(
+    private val firebaseRepository: FirebaseRepository
+) : BaseViewModel() {
+
+    fun signOut() = firebaseRepository.googleSignOut()
 
 }
